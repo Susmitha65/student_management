862f4fbb87c93189b688df0d011e076595e7df61
Susmitha V <148872403+Susmitha65@users.noreply.github.com>
2024-07-08 23:13:15 +0530

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import student_list, student_create, student_update
 
 urlpatterns = [
-    path('', student_list, name='student_list'),
-    path('create/', student_create, name='student_create'),
-    path('update/<int:pk>/', student_update, name='student_update'),
+    path('', views.index, name='index'),
+    path('student_list/',views.student_list name='student_list'),
+    path('create/', views.student_create, name='student_create'),
+    path('update/<int:pk>/', views.student_update, name='student_update'),
 ]
