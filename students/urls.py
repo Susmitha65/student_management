d77671fdd9d3f2f02ae949673554e2a071e85ac7
Susmitha V <148872403+Susmitha65@users.noreply.github.com>
2024-07-09 00:16:21 +0530

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('student_list/',views.student_list name='student_list'),
-    path('create/', views.student_create, name='student_create'),
+    path('add-student/', views.add_student, name='add_student'),
+    path('student-list/', views.student_list, name='student_list'),
     path('update/<int:pk>/', views.student_update, name='student_update'),
 ]
