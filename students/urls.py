c0b18c7b905b4927e685c1e2a911e6f14b937233
Susmitha65 <susmithavasdev@gmail.com>
2024-07-08 22:26:07 +0530

    Initial commit

---
+++ students/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import student_list, student_create, student_update
+
+urlpatterns = [
+    path('', student_list, name='student_list'),
+    path('create/', student_create, name='student_create'),
+    path('update/<int:pk>/', student_update, name='student_update'),
+]
